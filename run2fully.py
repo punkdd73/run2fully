0c88764c3089417eab88dc87e6d4f5d1afaa44ae
punkdd73 <butw.ec@gmail.com>
2026-05-20 14:35:28 +0800

    Update run2fully.py

--- run2fully.py
+++ run2fully.py
@@ -598,7 +598,7 @@ with st.sidebar:
      st.markdown("---")
     
     # 這是連結到您已準備好的靜態 HTML 文章
-    st.page_link("https://www.run2fully.com/blog/compound.html", label="寫在追尋複利之前", icon="☝️")
+     st.page_link("https://www.run2fully.com/blog/compound.html", label="寫在追尋複利之前", icon="☝️")
 
 st.markdown("---")
 st.caption("© 2026 Run2Fully. All rights reserved.")
