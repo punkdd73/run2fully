48ed2fb734b9fa208b3b1b468c022f6a68b04ffb
punkdd73 <butw.ec@gmail.com>
2026-06-02 15:49:14 +0800

    Update run2fully.py

--- run2fully.py
+++ run2fully.py
@@ -799,6 +799,7 @@ with st.sidebar:
      st.page_link("https://www.run2fully.com/blog/four_percent.html", label="**規劃你的4%永續提領**", icon="🔥")
      st.page_link("https://www.run2fully.com/blog/active_race.html", label="**少年不知「被動」好？**", icon="☝️")
      st.page_link("https://www.run2fully.com/blog/why_high_div.html", label="**到底該不該買高股息？**", icon="🔥")
+     st.page_link("https://www.run2fully.com/blog/dividend_truth.html", label="**「配息」的底層邏輯**", icon="🔥")
      st.page_link("https://www.run2fully.com/blog/why_high_div.html", label="**為何你必須強大？**", icon="☝️")
      st.page_link("https://www.run2fully.com/blog/try_short.html", label="**被動投資到底有多傻？**", icon="🔥")
      st.page_link("https://www.run2fully.com/blog/news_are dangerous.html", label="**消息的「保存期限」**", icon="☝️")
