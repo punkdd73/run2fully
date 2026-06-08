ccf37cd1afa639e7aaa39c1cd29baac9131a6897
punkdd73 <butw.ec@gmail.com>
2026-06-08 14:37:03 +0800

    Update run2fully.py

--- run2fully.py
+++ run2fully.py
@@ -803,6 +803,7 @@ with st.sidebar:
      st.page_link("https://www.run2fully.com/blog/why_high_div.html", label="**為何你必須強大？**", icon="☝️")
      st.page_link("https://www.run2fully.com/blog/try_short.html", label="**被動投資到底有多傻？**", icon="🔥")
      st.page_link("https://www.run2fully.com/blog/news_are dangerous.html", label="**消息的「保存期限」**", icon="☝️")
+     st.page_link("https://www.run2fully.com/blog/risky_risky.html", label="**你不自覺的「激進」了嗎？**", icon="☝️")
 
 
 
